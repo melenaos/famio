11b16dc8c7cf051de987e3caf9b947021d75d49a
melenaos <menelaosvergis@gmail.com>
2022-12-07 17:31:01 +0200

    main css

---
+++ src/components/panel.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="panel">
+    <h2>{{header}}</h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Panel',
+  props: {
+    record: {}
+  },
+  data () {
+    return {
+      message: 'Try change me!'
+    }
+  },
+  methods: {
+    onClick () {
+      this.message = 'Here you go :)'
+    }
+  }
+}
+</script>
+
+<style>
+.card {
+  padding: 20px;
+  background: #FFF;
+}
+</style>
